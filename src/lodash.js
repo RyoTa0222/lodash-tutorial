6cbd5519cf4fbbce92be0deddf21dd24eb28d0eb
RyoTa0222 <yotamana@gmail.com>
2021-05-31 08:34:52 +0900

    feature:components作成

--- src/lodash.js
+++ src/lodash.js
@@ -10,12 +10,24 @@ import {
   Box,
   Code
 } from '@chakra-ui/react';
+import Editor from "react-simple-code-editor";
 import AccordionTitle from './components/AccordionTitle';
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism.css"; //Example style, you can use another
 import _ from "lodash"
 
 const Loadash = () => {
-  const test = _.chunk(['a', 'b', 'c', 'd'], 3);
+  const test = _.chunk(['a', 'b', 'c', 'd', 'e'], 3);
   console.log(test)
+  const [code, setCode] = React.useState(`// example
+_.chunk(['a', 'b', 'c', 'd', 'e']);
+> [['a'], ['b'], ['c'], ['d'], ['e']]
+_.chunk(['a', 'b', 'c', 'd', 'e'], 2);
+> [['a', 'b'], ['c', 'd'], ['e']]
+_.chunk(['a', 'b', 'c', 'd', 'e'], 3);
+> [['a', 'b', 'c'], ['d', 'e']]`);
   return (
     <Container>
       <Accordion defaultIndex={[0]} mt="4" allowMultiple>
@@ -24,15 +36,24 @@ const Loadash = () => {
           <AccordionPanel pb={4}>
           {/* chunk */}
             <Heading fontSize="lg">chunk</Heading>
-            指定したサイズのグループに要素を分割した配列を作成します。配列を均等に分割できない場合、最後のチャンクは残りの要素になります。
+            引数のsizeで指定したサイズに要素を分割した配列を作成します。配列を均等に分割できない場合、最後のチャンクは残りの要素になります。
             <Box>
               引数
               array: Array
               size: number, default=1
             </Box>
-            <Code colorScheme="blackAlpha">
-              // example
-              _.chunk(['a', 'b', 'c', 'd'], 2);
+            <Code colorScheme="blackAlpha" width="100%">
+              <Editor
+                value={code}
+                onValueChange={(code) => setCode(code)}
+                highlight={(code) => highlight(code, languages.js)}
+                padding={10}
+                style={{
+                  fontWeight: 'bold',
+                  fontSize: 12,
+                  width: '100%'
+                }}
+              />
             </Code>
           </AccordionPanel>
         </AccordionItem>
